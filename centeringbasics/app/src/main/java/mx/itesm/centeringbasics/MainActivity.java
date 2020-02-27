ebf2e4206a9e63b6ea5d2cb46989582907fec225
Cesar Rodriguez <cesar.m.rodriguez@tec.mx>
2020-02-27 11:05:01 -0600

    update

---
+++ centeringbasics/app/src/main/java/mx/itesm/centeringbasics/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.itesm.centeringbasics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
